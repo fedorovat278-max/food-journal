08fef777c6451d21ee036aea03658e58ef7c5592
fedorovat278-max <fedorovat278@gmail.com>
2026-09-05 22:55:43 +0500

    Upgrade service worker to version 2
    Updated service worker to version 2 with improved caching and activation handling.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,49 @@
-const CACHE="food-journal-v1";
-const FILES=["./","./index.html","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "food-journal-v2";
+
+const FILES = [
+  "./app.html",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE)
+          .then(cache => {
+            cache.put(event.request, copy);
+          });
+
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request)
+      )
+  );
+});
